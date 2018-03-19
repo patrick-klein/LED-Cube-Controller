62742cd20c99a908f9426db2e78094f211c2862a
Patrick Klein <klein.pat@icloud.com>
2018-03-19 10:42:04 -0700

    add -f flag

--- compile.sh
+++ compile.sh
@@ -1,6 +1,5 @@
-rm ./obj/*
-rm ./bin/*
-
+rm -f ./obj/*
+rm -f ./bin/*
 
 clang++ -std=c++11 -stdlib=libc++ -c -I inc -o ./obj/main.o ./src/main.cpp
 
@@ -9,6 +8,5 @@ clang++ -std=c++11 -stdlib=libc++ -c -I inc -o ./obj/EdgeLight.o ./src/EdgeLight
 clang++ -std=c++11 -stdlib=libc++ -c -I inc -o ./obj/Raindrops.o ./src/Raindrops.cpp
 clang++ -std=c++11 -stdlib=libc++ -c -I inc -o ./obj/MovingPlane.o ./src/MovingPlane.cpp
 clang++ -std=c++11 -stdlib=libc++ -c -I inc -o ./obj/RandomToggle.o ./src/RandomToggle.cpp
-
 
 clang++ -std=c++11 -stdlib=libc++ ./obj/main.o ./obj/AnimateBullet.o ./obj/EdgeLight.o ./obj/Raindrops.o ./obj/MovingPlane.o ./obj/RandomToggle.o -Llib -I inc -liofunc -o ./bin/ledCube
